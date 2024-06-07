55dcb5f7a31d1cbe3d3b12ba3944a6e2107931a2
dhedal <hedgardavid@yahoo.fr>
2024-06-07 08:19:49 +0200

    initialisation du projet

---
+++ src/main/java/com/ecf/zevent/ZEventApplication.java
@@ -0,0 +1,13 @@
+package com.ecf.zevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZEventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecf/zevent/ZEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecf.zevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
