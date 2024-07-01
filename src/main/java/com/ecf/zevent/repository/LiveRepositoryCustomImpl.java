9936a153c6c053405e78645a81bba646bf40f10a
dhedal <hedgardavid@yahoo.fr>
2024-07-01 14:43:48 +0200

    correction des requetes jpa

--- src/main/java/com/ecf/zevent/repository/LiveRepositoryCustomImpl.java
+++ src/main/java/com/ecf/zevent/repository/LiveRepositoryCustomImpl.java
@@ -1,28 +1,35 @@
 package com.ecf.zevent.repository;
 
+import com.ecf.zevent.controller.LiveController;
 import com.ecf.zevent.model.Live;
 import com.ecf.zevent.model.Streamer;
 import com.ecf.zevent.model.ThematiqueType;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Parameter;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import jakarta.persistence.criteria.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class LiveRepositoryCustomImpl implements LiveRepositoryCustom {
+    private static final Logger LOG = LoggerFactory.getLogger(LiveRepositoryCustomImpl.class);
 
     @PersistenceContext
     private EntityManager entityManager;
 
+    /**
+     *
+     * @param date
+     * @param thematiqueType
+     * @param streamer
+     * @return
+     */
     @Override
     public List<Live> findLivesBy(LocalDate date, ThematiqueType thematiqueType, Streamer streamer) {
-        CriteriaBuilder cb = this.entityManager.getCriteriaBuilder();
-
-        StringBuilder rq = new StringBuilder("SELECT l FROM Live l WHERE ");
         List<String> conditions = new ArrayList<>();
 
         if(date != null) {
@@ -33,21 +40,80 @@ public class LiveRepositoryCustomImpl implements LiveRepositoryCustom {
             conditions.add("l.streamer = :streamer");
         }
 
-        if(thematiqueType != null) {
-            conditions.add("theme = :theme");
+        if(thematiqueType != null && !Objects.equals(ThematiqueType.NONE, thematiqueType)) {
+            conditions.add("l.themes LIKE concat('%', :theme, '%')");
         }
 
+        StringBuilder rq = new StringBuilder("SELECT l FROM Live l WHERE ");
         if(!conditions.isEmpty()) {
             rq.append(String.join(" AND ", conditions));
         }
 
+        CriteriaBuilder cb = this.entityManager.getCriteriaBuilder();
+
         Query query = this.entityManager.createQuery(rq.toString());
         if(date != null) query.setParameter("date", date);
         if(streamer != null) query.setParameter("streamer", streamer);
-        if(thematiqueType != null) query.setParameter("theme", thematiqueType);
+        if(thematiqueType != null) query.setParameter("theme", thematiqueType.getKey().toString());
 
-        System.out.println("***********************************************");
-        System.out.println(rq);
+        LOG.info("***********************************************");
+        LOG.info(rq.toString());
+        return query.getResultList();
+    }
+
+    /**
+     *
+     * @param thematiqueType
+     * @return
+     */
+    public List<Live> findLivesByTheme(ThematiqueType thematiqueType){
+        if(thematiqueType == null || Objects.equals(ThematiqueType.NONE, thematiqueType)) {
+            return List.of();
+        }
+        CriteriaBuilder cb = this.entityManager.getCriteriaBuilder();
+        StringBuilder rq = new StringBuilder("SELECT l FROM Live l WHERE l.themes LIKE concat('%', :theme, '%')");
+        Query query = this.entityManager.createQuery(rq.toString());
+        query.setParameter("theme", thematiqueType.getKey().toString());
+
+        LOG.info("***********************************************");
+        LOG.info(rq.toString());
+        return query.getResultList();
+
+    }
+
+    /**
+     *
+     * @param thematiqueTypes
+     * @return
+     */
+    @Override
+    public List<Live> findLivesByThemes(List<ThematiqueType> thematiqueTypes) {
+        if(thematiqueTypes == null || thematiqueTypes.isEmpty()) return List.of();
+
+        StringBuilder rq = new StringBuilder("SELECT DISTINCT l FROM Live l WHERE ");
+
+        List<String> conditions = new ArrayList<>();
+        Map<String, ThematiqueType> pMap = new HashMap<>();
+        String pName = "theme";
+        for(int i = 0; i < thematiqueTypes.size(); i++){
+            ThematiqueType thematiqueType = thematiqueTypes.get(i);
+            if(Objects.nonNull(thematiqueType) && !Objects.equals(ThematiqueType.NONE, thematiqueType)) {
+                String p = pName + i;
+                conditions.add("l.themes LIKE concat('%', :"+ p +", '%')");
+                pMap.put(p, thematiqueType);
+            }
+        }
+        if(conditions.isEmpty()) return List.of();
+        rq.append(String.join(" OR ", conditions));
+        LOG.info("***********************************************");
+        LOG.info(rq.toString());
+
+        Query query = this.entityManager.createQuery(rq.toString());
+        LOG.info(pMap.toString());
+        for(Map.Entry<String, ThematiqueType> entry: pMap.entrySet()) {
+            query.setParameter(entry.getKey(), entry.getValue().getKey().toString());
+        }
+
         return query.getResultList();
     }
 }
